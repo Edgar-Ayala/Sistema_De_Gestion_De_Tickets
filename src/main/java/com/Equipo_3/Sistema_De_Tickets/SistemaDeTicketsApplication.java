3f5ee0ccddec8e4058ea48135988417fbdc368f0
RonanRaw <ayalalereaedgarpablo@gmail.com>
2025-03-14 17:06:31 -0300

    Prueba para el envio hacia github

---
+++ src/main/java/com/Equipo_3/Sistema_De_Tickets/SistemaDeTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.Equipo_3.Sistema_De_Tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Equipo_3/Sistema_De_Tickets/SistemaDeTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Equipo_3.Sistema_De_Tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
